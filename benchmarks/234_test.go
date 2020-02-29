be05b5fff6be7310b6f4c2aea337ea573c9c36b6
chenxinlong <237448382@qq.com>
2020-02-29 10:36:54 +0800

    update:adj benchmark

--- benchmarks/234_test.go
+++ benchmarks/234_test.go
@@ -9,8 +9,16 @@ var (
 	input_234 = algs.ArrayToSingleLinkedList([]int{1,2,3,4,5,4,3,2,1})
 )
 
-func BenchmarkQ234Solution1(b *testing.B)  {
-	for i:=0; i<b.N; i++ {
-		algs.Q234Solution1(input_234)
-	}
+func BenchmarkQ234(b *testing.B) {
+	b.Run("solution1", func(b *testing.B) {
+		for i:=0; i<b.N; i++ {
+			algs.Q234Solution1(input_234)
+		}
+	})
+
+	b.Run("solution2", func(b *testing.B) {
+		for i:=0; i<b.N; i++ {
+			algs.Q234Solution2(input_234)
+		}
+	})
 }
--- benchmarks/876_test.go
+++ benchmarks/876_test.go
@@ -9,14 +9,16 @@ var (
 	input_876 = algs.ArrayToSingleLinkedList([]int{1,2,3,4,5,6,7,8,9,10})
 )
 
-func BenchmarkQ876Solution1(b *testing.B) {
-	for i := 0; i < b.N ; i++  {
-		algs.Q876Solution1(input_876)
-	}
-}
+func BenchmarkQ876(b *testing.B) {
+	b.Run("solution1", func(b *testing.B) {
+		for i:=0; i<b.N; i++ {
+			algs.Q876Solution1(input_876)
+		}
+	})
 
-func BenchmarkQ876Solution2(b *testing.B) {
-	for i := 0; i < b.N ; i++  {
-		algs.Q876Solution2(input_876)
-	}
+	b.Run("solution2", func(b *testing.B) {
+		for i:=0; i<b.N; i++ {
+			algs.Q876Solution2(input_876)
+		}
+	})
 }
